8506aaeb2972ae23fffd46d4d4e9a64edd4ebedc
StevenHH2000 <hlxhe2000@hotmail.com>
2025-04-21 09:26:01 +0000

    release the training code and data

--- qlora_to_merge.py
+++ qlora_to_merge.py
@@ -1,11 +1,3 @@
-'''
-Author: StevenHH2000 hlxhe2000@hotmail.com
-Date: 2025-04-14 11:49:26
-LastEditors: StevenHH2000 hlxhe2000@hotmail.com
-LastEditTime: 2025-04-18 02:09:18
-FilePath: /code/ICLR2025/training/qlora_to_merge.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
 # Merge the model
 from transformers import Idefics2ForConditionalGeneration, LlavaForConditionalGeneration, AutoProcessor
 from peft import PeftModel
@@ -14,13 +6,13 @@ import argparse
 
 def main(args):
     base_model = Idefics2ForConditionalGeneration.from_pretrained(args.base_model)
-    p_model = PeftModel.from_pretrained(base_model, model_id=args.qlora_model) # / 'checkpoint-602') #630')
+    p_model = PeftModel.from_pretrained(base_model, model_id=args.qlora_model)
     merge_model = p_model.merge_and_unload()
-    merge_model.save_pretrained(args.new_model) # / 'checkpoint-602') #630')
+    merge_model.save_pretrained(args.new_model)
     processor = AutoProcessor.from_pretrained(
-        args.base_model                                                                                                                                         , 
+        args.base_model,
         do_image_splitting=False)
-    processor.save_pretrained(args.new_model) # / 'checkpoint-602') #630')
+    processor.save_pretrained(args.new_model)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="This script is used for fine-tuning the IDEFICS2 model.")
@@ -33,13 +25,13 @@ if __name__ == "__main__":
     parser.add_argument(
         "--qlora_model",
         type=str,
-        default="checkpoints/idefics2-8b-pretrain-qlora-lr0.0002/checkpoint-602",
+        default="checkpoints/idefics2-8b-pretrain-qlora/checkpoint-602",
         help="Specify the name of the qlora weights.",
     )
     parser.add_argument(
         "--new_model",
         type=str,
-        default="checkpoints/idefics2-8b-pretrain-qlora-lr0.0002-merge",
+        default="checkpoints/idefics2-8b-pretrain-qlora-merge",
         help="Specify the name of the model after merging.",
     )
     
